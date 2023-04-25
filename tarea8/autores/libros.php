53fc96c1d28042a149c3a7ce077921e2ad11979f
naomi <124992962+naomi-pc@users.noreply.github.com>
2023-04-24 23:31:00 -0600

    Add files via upload

---
+++ tarea8/autores/libros.php
@@ -0,0 +1,8 @@
+<?php
+$authorId = $_GET['authorId'];
+$bookId = $_GET['bookId'];
+
+echo '<h4>Arrepentíos, Arlequín! Dijo el señor Tic Tac</h4>';
+echo '<p>Este es un libro maravilloso escrito por Harlan Ellison.</p>';
+echo '<p>Publicado en 1965, este libro cuenta la historia de un arlequín que se rebela contra un tirano que quiere controlar el tiempo.</p>';
+?>
